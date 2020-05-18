2ed9baccbcb6466aebc6125f4512461b6209dd14
longhoang08 <longhoang0811@gmail.com>
2020-05-18 23:28:37 +0700

    add api definition

---
+++ ecommerce-server/ecommerce_server/api/profile.py
@@ -0,0 +1,31 @@
+# coding=utf-8
+import logging
+
+import flask_restplus
+from flask import request
+
+from ecommerce_server import services
+from ecommerce_server.extensions import Namespace
+from ecommerce_server.services.user import check_permission
+from . import requests, responses
+
+__author__ = 'LongHB'
+_logger = logging.getLogger(__name__)
+
+ns = Namespace('profile', description='Profile operations')
+
+_change_req = ns.model('change_password_req', requests.change_password_req)
+_change_res = ns.model('change_password_res', responses.user_res)
+
+
+@ns.route('/change_password', methods=['GET', 'POST'])
+class Change_password(flask_restplus.Resource):
+    @ns.expect(_change_req, validate=True)
+    @ns.marshal_with(_change_res)
+    def post(self):
+        "validate user by current password and jwt token and set new password"
+        data = request.args or request.json
+        email = data.get('email')
+        check_permission(email)
+        new_user = services.password.change_password(**data)
+        return new_user
---
+++ ecommerce-server/ecommerce_server/api/register.py
@@ -0,0 +1,42 @@
+# coding=utf-8
+import logging
+
+import flask_restplus
+from flask import request
+
+from ecommerce_server import services
+from ecommerce_server.extensions import Namespace
+from ecommerce_server.helpers import decode_token
+from ecommerce_server.services.pending_register import send_confirm_email
+# from file_management.api import requests, responses
+from . import requests, responses
+
+__author__ = 'LongHB'
+_logger = logging.getLogger(__name__)
+
+ns = Namespace('register', description='Register operations')
+
+_register_req = ns.model('register_req', requests.register_user_req)
+_register_res = ns.model('register_res', responses.pending_register_res)
+
+
+@ns.route('/', methods=['GET', 'POST'])
+class Registers(flask_restplus.Resource):
+    @ns.expect(_register_req, validate=True)
+    @ns.marshal_with(_register_res)
+    def post(self):
+        "validate register data, add data to pending register table and send confirm email"
+        data = request.args or request.json
+        pending_register = services.pending_register.create_pending_register(**data)
+        send_confirm_email(**data)
+        return pending_register
+
+
+@ns.route('/confirm_email/<token>', methods=['GET'])
+class Confirm_email(flask_restplus.Resource):
+
+    def get(self, token):
+        "checking jwt token in param and add new user to user table"
+        email = decode_token(token)
+        user = services.user.confirm_user_by_email(email)
+        return user.to_display_dict()
