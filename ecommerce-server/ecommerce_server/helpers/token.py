ab48f34c515ed7e9f6b8e939fda98cf2cf44637f
longhoang08 <longhoang0811@gmail.com>
2020-05-19 20:22:27 +0700

    fix jwt custom lib

--- ecommerce-server/ecommerce_server/helpers/token.py
+++ ecommerce-server/ecommerce_server/helpers/token.py
@@ -1,10 +1,13 @@
 import datetime
-import os
 
 import jwt
 
+from ecommerce_server.helpers import get_environ
 
-def encode_token(email, minute):
+SECRET_KEY = get_environ('SECRET_KEY')
+
+
+def encode_token(email, minute) -> str:
     from ecommerce_server.extensions.custom_exception import EncodeErrorException
 
     try:
@@ -14,17 +17,17 @@ def encode_token(email, minute):
         }
         return jwt.encode(
             payload,
-            os.environ['SECRET_KEY'],
+            SECRET_KEY,
             algorithm='HS256'
         ).decode("utf-8")
     except Exception as e:
         raise EncodeErrorException()
 
 
-def decode_token(auth_token):
+def decode_token(auth_token: str) -> str:
     from ecommerce_server.extensions.custom_exception import InvalidTokenException
     try:
-        payload = jwt.decode(auth_token, os.environ['SECRET_KEY'])
+        payload = jwt.decode(auth_token, SECRET_KEY)
         return payload['email']
     except:
         raise InvalidTokenException()
