fa1aeed1ec85f691387200a6fb5d319eb61ec6cc
longhoang08 <longhoang0811@gmail.com>
2020-05-19 20:22:54 +0700

    PEP 8 style

--- ecommerce-server/ecommerce_server/api/register.py
+++ ecommerce-server/ecommerce_server/api/register.py
@@ -34,7 +34,7 @@ class Registers(flask_restplus.Resource):
 @ns.route('/confirm_email/<token>', methods=['GET'])
 class Confirm_email(flask_restplus.Resource):
 
-    def get(self, token):
+    def get(self, token: str):
         "checking jwt token in param and add new user to user table"
         email = decode_token(token)
         user = services.user.confirm_user_by_email(email)
