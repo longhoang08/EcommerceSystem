5f477ac57a448b6b9359e6ceac78f449c4c24fd1
longhoang08 <longhoang0811@gmail.com>
2020-05-19 17:14:21 +0700

    update pending register base on new logic

--- ecommerce-server/ecommerce_server/services/pending_register.py
+++ ecommerce-server/ecommerce_server/services/pending_register.py
@@ -14,43 +14,31 @@ __author__ = 'LongHB'
 _logger = logging.getLogger(__name__)
 
 
-def create_pending_register(username, email, fullname, password, **kwargs):
-    if validate_register(username, email, fullname, password):
-        existed_user = repo.user.find_one_by_email_or_username_ignore_case(
-            email, username)
-        # check if user existed
-        if existed_user:
-            message = ("User with username {username} " +
-                       "or email {email} already existed!").format(
-                username=username,
-                email=email
-            )
-            raise UserExistsException(message)
-        # check if that user registed before
-        existed_pending_register = repo.pending_register.find_one_by_email_or_username(email, username)
-        if (existed_pending_register):
-            message = ("User with username {username} " +
-                       "or email {email} already register before").format(
-                username=username,
-                email=email
-            )
-            raise RegisterBeforeException(message)
-
-        # save pending register to database
-        pending_register = repo.pending_register.save_pending_register_to_database(
-            username=username,
-            email=email,
-            fullname=fullname,
-            password=hash_password(password)
-        )
-        return pending_register
-
-    else:
+def create_pending_register(email, fullname, phone_number, password, **kwargs):
+    if not validate_register(email, fullname, password):
         raise BadRequestException("Invalid user data specified!")
 
+    existed_user = repo.user.find_one_by_email_or_phone_number_ignore_case(
+        email, phone_number)
 
-def send_confirm_email(username, email, fullname, **kwargs):
+    if existed_user:
+        raise UserExistsException()
+
+    existed_pending_register = repo.pending_register.find_one_by_email_or_phone_number(email, phone_number)
+    if existed_pending_register:
+        raise RegisterBeforeException()
+
+    pending_register = repo.pending_register.save_pending_register_to_database(
+        email=email,
+        fullname=fullname,
+        phone_number=phone_number,
+        password=hash_password(password)
+    )
+    return pending_register
+
+
+def send_confirm_email(email, fullname, **kwargs):
     confirm_token = encode_token(email, int(os.environ['TOKEN_UPTIME']))
     active_link = os.environ['SERVER_BASE_URL'] + 'register/confirm_email/' + confirm_token
-    msg_html = email_template.gen_confirm_email_body_template(fullname, username, active_link)
-    mail_service.send_email("uFile's email confirmation", email, msg_html)
+    msg_html = email_template.gen_confirm_email_body_template(fullname, email, active_link)
+    mail_service.send_email("uShop's email confirmation", email, msg_html)
