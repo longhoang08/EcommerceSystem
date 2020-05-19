3ddf2bc7e20c56d999a339a1d18d31ceff04c989
longhoang08 <longhoang0811@gmail.com>
2020-05-19 16:03:01 +0700

    update email template, change to uShop

--- ecommerce-server/ecommerce_server/templates/email_template.py
+++ ecommerce-server/ecommerce_server/templates/email_template.py
@@ -5,12 +5,15 @@ __author__ = 'LongHB'
 _logger = logging.getLogger(__name__)
 
 
-def gen_confirm_email_body_template(fullname, username, active_link):
-    msg_html = ('<p>Hello {},</p>'
-                '<p>You are almost ready to start using uFile with handle {}.</p>'
+def gen_confirm_email_body_template(fullname: str, username: str, active_link: str):
+    fullname = "" if not fullname else (' ' + fullname)
+
+    msg_html = ('<p>Hello{},</p>'
+                '<p>You are almost ready to start using uShop with email {}.</p>'
                 "<p>Simply click <a href= '{}' >link</a> to verify your accout!</p>"
                 '<p>Best regards,</p>'
-                "<p>uFile team</p>")
+                "<p>uShop team</p>")
+
     msg_html = msg_html.format(fullname, username, active_link)
     # msg_html = msg_html.replace("'", '"')
     return msg_html
