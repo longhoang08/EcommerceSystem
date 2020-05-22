312713edaa20c87550540d458a63899294ba3744
longhoang08 <longhoang0811@gmail.com>
2020-05-22 13:48:39 +0700

    add file service and file repository

--- ecommerce-server/ecommerce_server/services/file.py
+++ ecommerce-server/ecommerce_server/services/file.py
@@ -1,23 +1,16 @@
 # coding=utf-8
 import logging
 
-from ecommerce_server.repositories.product import ProductElasticRepo
-
 __author__ = 'LongHB'
+
+from werkzeug.datastructures import MultiDict
+
+from ecommerce_server.repositories import file as repo
+
 _logger = logging.getLogger(__name__)
 
 
-def get_all_files(args):
-    file_es = ProductElasticRepo()
-    responses = file_es.search(args)
-    files = extract_file_data_from_response(responses)
-    return files
-
-
-def extract_file_data_from_response(responses):
-    if not responses:
-        return {'result': {'files': []}}
-    responses = responses.to_dict()
-    hits = responses['hits']['hits']
-    files = [item['_source'] for item in hits]
-    return {'result': {'files': files}}
+def upload_image(images: MultiDict):
+    return {
+        'image-url': repo.upload_image(images)
+    }
