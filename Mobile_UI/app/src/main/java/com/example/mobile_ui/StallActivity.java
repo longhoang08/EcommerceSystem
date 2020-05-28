aeeb0a4647f263d588ade94328dc50370c451bac
Nguyễn Thành An <thanhannguyen211@gmail.com>
2020-05-28 19:03:58 +0700

    screen product in the stall

--- Mobile_UI/app/src/main/java/com/example/mobile_ui/StallActivity.java
+++ Mobile_UI/app/src/main/java/com/example/mobile_ui/StallActivity.java
@@ -6,15 +6,47 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.example.mobile_ui.Adapter.ProductStallAdapter;
+import com.example.mobile_ui.Model.Product;
+
+import java.util.ArrayList;
 
 public class StallActivity extends AppCompatActivity {
-    Button btnAddProduct;
+    TextView btnAddProduct;
+    TextView showBuyRecord;
+    ListView stallProListView;
+    ImageView stall_infor_img_main;
+    TextView userName;
+    TextView numOfPro;
+
+    ArrayList<Product> products = new ArrayList<Product>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stall);
         //anh xa
         btnAddProduct = findViewById(R.id.btn_add_product);
+        showBuyRecord = findViewById(R.id.stallBuyRecord);
+        stallProListView = findViewById(R.id.stallProListView);
+        stall_infor_img_main = findViewById(R.id.stall_infor_img_main);
+        userName = findViewById(R.id.userName);
+        numOfPro = findViewById(R.id.numOfPro);
+
+        //fake data
+        products.add(new Product(R.drawable.icon_kiwi_fruit,"banana",12000,120));
+        products.add(new Product(R.drawable.icon_kiwi_fruit,"apple",12000,100));
+        products.add(new Product(R.drawable.icon_kiwi_fruit,"lemon",12000,80));
+        products.add(new Product(R.drawable.icon_kiwi_fruit,"suit",12000,60));
+
+        ProductStallAdapter adapter = new ProductStallAdapter(
+                StallActivity.this,R.layout.stall_product_item,products
+        );
+        stallProListView.setAdapter(adapter);
+
         btnAddProduct.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
