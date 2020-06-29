27d07fd8f9c6f347965ba0eb08235bb761706031
Truong-AQ <nguyenvantruonglopso9@gmail.com>
2020-06-29 10:00:42 +0700

    updateProfile

--- Mobile_UI/app/src/main/java/com/example/mobile_ui/Retrofit/APIUtils.java
+++ Mobile_UI/app/src/main/java/com/example/mobile_ui/Retrofit/APIUtils.java
@@ -1,7 +1,8 @@
 package com.example.mobile_ui.Retrofit;
 
 //gui nhan du lieu ve
-public class APIUtils {
+public class
+APIUtils {
     public static final String Base_Url="http://112.137.129.216:5001/api/";
 
     public static DataClient getData(){
--- Mobile_UI/app/src/main/java/com/example/mobile_ui/SettingAccountActivity.java
+++ Mobile_UI/app/src/main/java/com/example/mobile_ui/SettingAccountActivity.java
@@ -1,14 +1,22 @@
 package com.example.mobile_ui;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.app.AlertDialog;
 import android.app.DatePickerDialog;
+import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
@@ -47,12 +55,27 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
 import java.sql.SQLOutput;
 import java.util.HashMap;
 import java.util.Map;
+
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.OkHttpClient;
+import okhttp3.RequestBody;
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.Multipart;
+import retrofit2.http.POST;
+import retrofit2.http.Part;
 
 public class SettingAccountActivity extends AppCompatActivity {
     Button changeOrSave;
@@ -60,6 +83,7 @@ public class SettingAccountActivity extends AppCompatActivity {
     ImageView user_infor_img;
     Button touchToChangeImg;
     EditText user_name;
+    int countRequest = 0;
 //    RadioGroup radioSex;
 //    RadioButton radioMale;
 //    RadioButton radioFemale;
@@ -73,8 +97,15 @@ public class SettingAccountActivity extends AppCompatActivity {
 
     final int REQUEST_CODE_CAMERA=123;
     final int REQUEST_CODE_FOLDER=456;
+    final int REQUEST_CALL_RW = 234;
+    Uri uri;
+    boolean changeImg = false;
+    ProgressDialog pd;
 
     private void anhxa(){
+        RequestQueue queue = Volley.newRequestQueue(SettingAccountActivity.this);
+        pd = new ProgressDialog(SettingAccountActivity.this);
+        pd.setTitle("Chờ 1 lát ...");
         changeOrSave = findViewById(R.id.changeOrSave);changeOrSave.setText("change");
         container_user_img = findViewById(R.id.container_user_img);
         user_infor_img = findViewById(R.id.user_infor_img);
@@ -96,8 +127,14 @@ public class SettingAccountActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 //giao diện
-                final chooseImage chooseImageProfile = new chooseImage();
-                chooseImageProfile.show(getSupportFragmentManager(), "");
+                if (ContextCompat.checkSelfPermission(SettingAccountActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE)
+                        != PackageManager.PERMISSION_GRANTED) {
+                    // request the permission - cấp quyền
+                    ActivityCompat.requestPermissions(SettingAccountActivity.this, new String[] {Manifest.permission.READ_EXTERNAL_STORAGE}, REQUEST_CALL_RW);
+                } else {
+                    final chooseImage chooseImageProfile = new chooseImage();
+                    chooseImageProfile.show(getSupportFragmentManager(), "");
+                }
                 //set sự kiện
 //                takePhoto.setOnClickListener(new View.OnClickListener() {
 //                    @Override
@@ -194,11 +231,89 @@ public class SettingAccountActivity extends AppCompatActivity {
     private void postData(){
 //        int selectedId = radioSex.getCheckedRadioButtonId();
 //        RadioButton radioButton = (RadioButton) findViewById(selectedId);
-
-        Toast.makeText(SettingAccountActivity.this,"lưu thành công !",Toast.LENGTH_SHORT).show();
+        pd.show();
+        countRequest ++;
+        upDateInfo(address.getText().toString(), "");
+        if (changeImg) {
+            countRequest++;
+            uploadFileServer();
+        }
+//        Toast.makeText(SettingAccountActivity.this,"lưu thành công !",Toast.LENGTH_SHORT).show();
 
     }
 
+    public void upDateInfo(String address, String urlImg) {
+        JSONObject params = new JSONObject();
+        if (urlImg.equals("")) {
+            try {
+                params.put("address", address);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        if (address.equals("")) {
+            try {
+                params.put("avatar_url", urlImg);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        final String url = "http://112.137.129.216:5001/api/profile/update_profile";
+        JsonObjectRequest jsonObjReq = new JsonObjectRequest(
+                Request.Method.POST, url, params,
+                new com.android.volley.Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        countRequest --;
+//                        user_name.setText(countRequest+response.toString());
+                        if (countRequest == 0) {
+                            pd.cancel();
+                            final AlertDialog.Builder builder = new AlertDialog.Builder(SettingAccountActivity.this);
+                            builder.setTitle("Thông báo");
+                            builder.setMessage("Cập nhật thành công.");
+                            builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    dialog.cancel();
+                                    finish();
+                                }
+                            });
+                            AlertDialog alertDialog = builder.create();
+                            alertDialog.show();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+//                Toast.makeText(SettingAccountActivity.this, "error", Toast.LENGTH_LONG).show();
+//                System.out.println("error");
+                NetworkResponse response = error.networkResponse;
+                if (response != null) {
+
+                    try {
+                        String res = new String(response.data,
+                                HttpHeaderParser.parseCharset(response.headers, "utf-8"));
+//                        user_name.setText(res);
+                    } catch (UnsupportedEncodingException e1) {
+                        e1.printStackTrace();
+                    }
+                }
+            }
+        })
+        {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                HashMap<String, String> headers = new HashMap<String, String>();
+                headers.put("Content-Type", "application/json");
+                SharedPreferences sharedPreferences = SettingAccountActivity.this.getSharedPreferences("VALUABLE_APP", Context.MODE_PRIVATE);
+                String token = sharedPreferences.getString("LOGIN_TOKEN","");
+                headers.put("x-access-token", token);
+                return headers;
+            }
+        };
+        Volley.newRequestQueue(SettingAccountActivity.this).add(jsonObjReq);
+    }
     //set sự kiện khi click vào thay đổi ảnh + thay đổi ngày sinh
     /*private void setOnClick(ImageView img,TextView txt,boolean bl){
         if(!bl){
@@ -294,7 +409,9 @@ public class SettingAccountActivity extends AppCompatActivity {
             user_infor_img.setImageBitmap(bitmap);
             return;
         }else if(requestCode==REQUEST_CODE_FOLDER && resultCode ==RESULT_OK && data!=null){
-            Uri uri = data.getData();
+            uri = data.getData();
+            changeImg = true;
+//            uploadFileServer();
             try {
                 InputStream inputStream = getContentResolver().openInputStream(uri);
                 Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
@@ -306,9 +423,9 @@ public class SettingAccountActivity extends AppCompatActivity {
         }
     }
 
+
     private void getUser() {
         // call api get status
-        RequestQueue queue = Volley.newRequestQueue(SettingAccountActivity.this);
         JSONObject params = new JSONObject();
 
         final String url = "http://112.137.129.216:5001/api/users/get_status";
@@ -372,5 +489,82 @@ public class SettingAccountActivity extends AppCompatActivity {
             }
         };
         Volley.newRequestQueue(SettingAccountActivity.this).add(jsonObjReq);
+    }
+
+    // 4 function de upload file len server
+    public interface UploadService {
+        @Multipart
+        @POST("api/file/store-image")
+        Call<ResponseBody> uploadFile(@Part MultipartBody.Part file);
+    }
+    private void uploadFileServer() {
+        if (uri == null) return;
+        // Hàm call api sẽ mất 1 thời gian nên mình show 1 dialog nhé.
+
+        File file = new File(getRealPathFromURI(uri));
+        // Khởi tạo RequestBody từ file đã được chọn
+        RequestBody requestBody = RequestBody.create(
+                MediaType.parse(getContentResolver().getType(uri)),
+                file);
+        // Trong retrofit 2 để upload file ta sử dụng Multipart, khai báo 1 MultipartBody.Part
+        // uploaded_file là key mà mình đã định nghĩa trong khi khởi tạo server
+        MultipartBody.Part filePart =
+                MultipartBody.Part.createFormData("image", file.getName(), requestBody);
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://112.137.129.216:5001/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(new OkHttpClient())
+                .build();
+        UploadService service = retrofit.create(UploadService.class);
+        Call<ResponseBody> call = service.uploadFile(filePart);
+        call.enqueue(new Callback<ResponseBody>() {
+            @Override
+            public void onResponse(Call<ResponseBody> call, retrofit2.Response<ResponseBody> response) {
+
+//                user_name.setText(response.toString());
+//                    Toast.makeText(SettingAccountActivity.this, response.toString(), Toast.LENGTH_LONG).show();
+                if (response == null || response.body() == null) {
+                    return;
+                }
+                try {
+                    String responseUrl = response.body().string();
+                    JSONObject jsonObject = new JSONObject(responseUrl);
+                    String urlImg = (String) jsonObject.get("image-url");
+                    upDateInfo("", urlImg);
+//                        Toast.makeText(SettingAccountActivity.this, responseUrl, Toast.LENGTH_LONG).show();
+                } catch (IOException | JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+
+            @Override
+            public void onFailure(Call<ResponseBody> call, Throwable t) {
+//                    Toast.makeText(SettingAccountActivity.this, t.getMessage()+"|abcd", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    private String getRealPathFromURI(Uri uri) {
+        String result;
+        Cursor cursor = getContentResolver().query(uri, null, null, null, null);
+        if (cursor == null) {
+            result = uri.getPath();
+        } else {
+            cursor.moveToFirst();
+            int idx = cursor.getColumnIndex(MediaStore.Images.ImageColumns.DATA);
+            result = cursor.getString(idx);
+            cursor.close();
+        }
+        return result;
+    }
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_CALL_RW) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                final chooseImage chooseImageProfile = new chooseImage();
+                chooseImageProfile.show(getSupportFragmentManager(), "");
+            }
+        }
     }
 }
